b92a58ffc0969791ace4b75faef7a2d50519735c
hustle-dev <dlwoabsdk@gmail.com>
2022-02-07 01:52:11 +0900

    feat: App 파일 컴포넌트 불러오기
    마크업 테스트 확인을 위한 App 파일에 컴포넌트 불러오기

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
-import './App.css';
+import { Header, A11yHidden, GameInfo, Board, BoardItem } from 'components';
 
 export default function App(): JSX.Element {
-  return <div className="App">hello React</div>;
+  const style = {
+    width: '176px',
+    height: '176px',
+    margin: '2px',
+    backgroundColor: 'rgba(224, 153, 43)',
+  };
+
+  // 상태: isPlaying, stage, baseColor, answerColor, score, time, onSelect
+
+  return (
+    <>
+      <Header>
+        <A11yHidden as="h1">다른 색깔 찾기 게임</A11yHidden>
+        <GameInfo />
+      </Header>
+      <Board>
+        <BoardItem style={style} />
+        <BoardItem style={style} />
+        <BoardItem style={style} />
+        <BoardItem style={style} />
+      </Board>
+    </>
+  );
 }
