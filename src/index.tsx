67ad2244f35abbedb7af69ce90eeac0180a5dd81
hustle-dev <dlwoabsdk@gmail.com>
2022-02-10 01:34:14 +0900

    fix: App폴더 제거 및 파일 위치 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 import 'styles/global.css';
-import App from './App/App';
+import App from 'App';
 
 render(
   <StrictMode>
